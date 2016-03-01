1ad4e538b0426364bad6e863acb2fcfffa92ff56
Bruno Paré-Simard <bruno.paresimard@gmail.com>
2016-03-01 18:28:02 -0500

    Javascript now act similary to Select2, it require user activation.

--- Resources/public/js/select24entity.js
+++ Resources/public/js/select24entity.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
   $.fn.select24entity = function (action) {
-    if (action) {
-      this.select2(action);
-      return this;
-    }
-    this.select2({
+    // Create the parameters array with basic values
+    var select24entityParam = {
       ajax: {
         data: function (params) {
           return {
@@ -18,9 +15,15 @@ $(document).ready(function () {
         },
         cache: true
       }
-    });
+    };
+
+    // Extend the parameters array with the one in arguments
+    $.extend(select24entityParam, action);
+
+    // Activate the select2 field
+    this.select2(select24entityParam);
+
+    // Return current field
     return this;
   };
-
-  $('.select24entity').select24entity();
 });
